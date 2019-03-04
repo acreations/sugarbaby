82b9de225e066f5f092ebf55fe451ac02a4948a3
acreations@ultima <aaron.wong.home@gmail.com>
2019-03-04 21:43:52 +0100

    Added run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FOLDER=$(date +%Y)
+TARGET=$(date +%Y/%Y-%m-%d)
+
+if [ ! -d "${FOLDER}" ]; then
+    mkdir -p "${FOLDER}"
+fi
+
+cd /opt/apps/sugardaddy
+
+bash ./run.sh > "/opt/apps/sugarbaby/${TARGET}"
+
+cd -
